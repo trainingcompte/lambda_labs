93dc6d011c8268d5b303a566498f9bc897719d05
trainingcompte <134267460+trainingcompte@users.noreply.github.com>
2023-05-30 09:57:49 +0100

    Update testlambdafunction.py

--- Simple_Lambda/testlambdafunction.py
+++ Simple_Lambda/testlambdafunction.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
     print(' C est ma fonction AWS Lambda pour juste tester')
     if event['planete'] == 'Terre':
         return 'Lune'
-    elif event['planete'] == 'Soleil'
+    elif event['planete'] == 'Soleil':
         return 'C n est pas une planete'
     else:
         return 'Je ne comprends pas votre argument!!'
